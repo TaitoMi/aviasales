e7f2dc341ef4538eeac3af6336fe698a3163f5c2
TaitoMi <gfdaker96@gmail.com>
2019-12-02 21:46:11 +0300

    some html and css in js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import 'normalize.css';
-import { Tabs, Icon } from 'antd';
-import 'antd/dist/antd.css';
-import Timer from './components/Timer';
-import CountDown from './components/CountDown';
-import './styles/app.scss';
+import logo from './images/Logo.png';
+import ContainerFluid from './components/styled/ContainerFluid';
+import LogoLink from './components/styled/LogoLink';
+import Content from './components/styled/Content';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,42 +13,15 @@ class App extends React.Component {
     };
   }
 
-  toCountdown = () => {
-    const { isTimer } = this.state;
-    this.setState({ isTimer: !isTimer });
-  };
-
   render() {
     const { isTimer } = this.state;
     return (
-      <div className="app">
-        <Tabs defaultActiveKey="1" onChange={this.toCountdown}>
-          <Tabs.TabPane
-            className="app__tab"
-            tab={
-              <span>
-                <Icon type="clock-circle" />
-                Timer
-              </span>
-            }
-            key="1"
-          >
-            {isTimer && <Timer />}
-          </Tabs.TabPane>
-          <Tabs.TabPane
-            className="app__tab"
-            tab={
-              <span>
-                <Icon type="dashboard" />
-                CountDown
-              </span>
-            }
-            key="2"
-          >
-            {!isTimer && <CountDown />}
-          </Tabs.TabPane>
-        </Tabs>
-      </div>
+      <ContainerFluid>
+        <LogoLink>
+          <img src={logo} alt="logo" />
+        </LogoLink>
+        <Content> {isTimer.toString()} </Content>
+      </ContainerFluid>
     );
   }
 }
---
+++ src/components/styled/ContainerFluid.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f3f7fa;
+  width: 100vw;
+  height: 100vh;
+  padding-top: 40px;
+`;
