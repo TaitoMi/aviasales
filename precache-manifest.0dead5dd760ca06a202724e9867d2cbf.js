214679f3868300e4f2c4a2ac9d754eabff2a1da4
TaitoMi <gfdaker96@gmail.com>
2019-12-05 10:24:17 +0300

    Updates

---
+++ precache-manifest.0dead5dd760ca06a202724e9867d2cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e8e20e1650688aadfdf983ef8eb34f",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "8f4fedb82f05d69206d0",
+    "url": "/aviasales/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8f4fedb82f05d69206d0",
+    "url": "/aviasales/static/js/2.a8c62557.chunk.js"
+  },
+  {
+    "revision": "98430615fab46cf71de6",
+    "url": "/aviasales/static/js/main.7f91f431.chunk.js"
+  },
+  {
+    "revision": "6869b8c0c7d5aa8946a5",
+    "url": "/aviasales/static/js/runtime-main.65de5ece.js"
+  },
+  {
+    "revision": "d2a4807b0aa4a7ca1e7a82ee730e6fb6",
+    "url": "/aviasales/static/media/Logo.d2a4807b.png"
+  }
+]);
